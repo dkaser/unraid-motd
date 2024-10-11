093d872e9cc72136d0b2be321a1192ab69c8fe69
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2024-10-11 00:33:20 +0000

    add filters for services

--- datasources/services.go
+++ datasources/services.go
@@ -4,6 +4,11 @@ import (
 	"github.com/dkaser/unraid-motd/utils"
 	"os/exec"
 	"regexp"
+	"os"
+	"errors"
+	"gopkg.in/ini.v1"
+	"strings"
+	"fmt"
 )
 
 type ConfServices struct {
@@ -41,15 +46,52 @@ func getServiceStatus(sourceConf *ConfServices) (content string) {
 
 	overall := utils.Good("OK")
 
+	ident, err := ini.Load("/boot/config/ident.cfg")
+    if err != nil {
+        fmt.Printf("Fail to read file: %v", err)
+    }
+
+	shares, err := ini.Load("/boot/config/share.cfg")
+    if err != nil {
+        fmt.Printf("Fail to read file: %v", err)
+    }
+
+	docker, err := ini.Load("/boot/config/docker.cfg")
+    if err != nil {
+        fmt.Printf("Fail to read file: %v", err)
+    }
+
 	//SERVICES:
 	for _, service := range sourceConf.Services {
 		reg := regexp.MustCompile(`[^a-zA-Z0-9\-_]+`)
 		service = reg.ReplaceAllString(service, "")
 
+		switch service {
+			case "sshd":
+				if (strings.ToLower(ident.Section("").Key("USE_SSH").String()) == "no") {
+					continue
+				}
+			case "nfsd":
+				if (strings.ToLower(shares.Section("").Key("shareNFSEnabled").String()) == "no") {
+					continue
+				}
+			case "samba":
+				if (strings.ToLower(shares.Section("").Key("shareSMBEnabled").String()) == "no") {
+					continue
+				}
+			case "docker":
+				if (strings.ToLower(docker.Section("").Key("DOCKER_ENABLED").String()) == "no") {
+					continue
+				}
+		}
+
 		cmd := exec.Command("/etc/rc.d/rc."+service, "status") // #nosec G204
 		err := cmd.Run()
 
 		if err != nil {
+			if errors.Is(err, os.ErrNotExist) {
+				continue
+			}
 			overall = utils.Err("Critical")
 			outputTable.AppendRow([]interface{}{service, utils.Err("Not running")})
 		} else if !*sourceConf.WarnOnly {
--- go.mod
+++ go.mod
@@ -52,5 +52,6 @@ require (
 	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
 	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
 	gotest.tools/v3 v3.0.3 // indirect
 )
